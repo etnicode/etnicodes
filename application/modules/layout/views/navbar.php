e14ce6d7c13fbadbe5dd9865cf1d730c007ba1da
Muhamad Anik <anikbusinessman@gmail.com>
2020-07-24 22:57:45 +0700

    Add Image

--- application/modules/layout/views/navbar.php
+++ application/modules/layout/views/navbar.php
@@ -11,12 +11,12 @@
 
         <nav class="nav-menu d-none d-lg-block">
             <ul>
-                <li class="active"><a href="#index.html">Home</a></li>
+                <li class="active"><a href="<?= base_url(); ?>">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#services">Services</a></li>
                 <li><a href="#portfolio">Portfolio</a></li>
                 <li><a href="#team">Team</a></li>
-                <!-- <li><a href="#pricing">Pricing</a></li> -->
+                <li><a href="https://www.blog.etnicode.com">Blog</a></li>
                 <li class="drop-down"></li>
 
                 <li><a href="#contact">Contact</a></li>
